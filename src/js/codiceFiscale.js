4edb5341a1ff13e37c0c551d872bc7321e528883
andreachicco <andreachicco2002.ac@gmail.com>
2022-04-05 08:00:14 +0200

    risolto problema calcolo lettera di controllo

--- src/js/codiceFiscale.js
+++ src/js/codiceFiscale.js
@@ -27,7 +27,7 @@ async function getTownFromFile(inputTown, fileName){
 const vowels = ['a', 'e', 'i', 'o', 'u'];
 const monthLetters = ['a', 'b', 'c', 'd', 'e', 'h', 'l', 'm', 'p', 'r', 's', 't'];
 
-const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 const alphanumericChars = {
     odd: {
